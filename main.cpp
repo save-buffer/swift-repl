114d9beae59a452c604b2aab6bfa7927eb2130dc
Sasha Krassovsky <sashakrassovsky@fb.com>
2019-07-19 11:10:00 -0700

    Create REPL struct

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include <string>
 
+struct REPL
+{
+    bool IsExitString(const std::string &line)
+    {
+        return line == "e" || line == "exit";
+    }
+
+    bool ExecuteSwift(std::string line)
+    {
+        if(IsExitString(line))
+            return false;
+        return true;
+    }
+};
+
 int main(int argc, char **argv)
 {
     std::string line;
-    for(;;)
+    REPL repl;
+    do
     {
         std::getline(std::cin, line);
-        if(line == "e" || line == "exit")
-        {
-            return 0;
-        }
-    }
+    } while(repl.ExecuteSwift(line));
     return 0;
 }
