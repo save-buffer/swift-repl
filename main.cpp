db4b5ef815102ffab4abf75252372753f6e91237
Sasha Krassovsky <sashakrassovsky@fb.com>
2019-07-19 11:19:36 -0700

    Create AST Context with diagnostics

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 #include <string>
 
+#include <swift/AST/ASTContext.h>
+#include <swift/AST/DiagnosticConsumer.h>
+#include <swift/AST/DiagnosticEngine.h>
+#include <swift/Basic/LangOptions.h>
+#include <swift/Basic/SourceManager.h>
+
+class PrinterDiagnosticConsumer : public swift::DiagnosticConsumer
+{
+    void handleDiagnostic(swift::SourceManager &src_mgr,
+                          swift::SourceLoc loc,
+                          swift::DiagnosticKind kind,
+                          llvm::StringRef fmt_str,
+                          llvm::ArrayRef<swift::DiagnosticArgument> fmt_args,
+                          const swift::DiagnosticInfo &info,
+                          const swift::SourceLoc)
+    {
+        std::string diagnostic;
+        llvm::raw_string_ostream stream(diagnostic);
+        swift::DiagnosticEngine::formatDiagnosticText(stream, fmt_str, fmt_args);
+        stream.flush();
+        std::cout << "Received Diagnostic: " << diagnostic << std::endl;
+    }
+};
+
 struct REPL
 {
+    REPL() : m_diagnostic_engine(m_src_mgr),
+             m_ast_ctx(swift::ASTContext::get(m_lang_opts,
+                                              m_spath_opts,
+                                              m_src_mgr,
+                                              m_diagnostic_engine))
+    {
+        m_diagnostic_engine.setShowDiagnosticsAfterFatalError();
+        m_diagnostic_engine.addConsumer(m_diagnostic_consumer);
+    }
+
     bool IsExitString(const std::string &line)
     {
         return line == "e" || line == "exit";
@@ -14,6 +48,15 @@ struct REPL
             return false;
         return true;
     }
+
+    swift::LangOptions m_lang_opts;
+    swift::SearchPathOptions m_spath_opts;
+
+    swift::SourceManager m_src_mgr;
+    swift::DiagnosticEngine m_diagnostic_engine;
+    PrinterDiagnosticConsumer m_diagnostic_consumer;
+
+    std::unique_ptr<swift::ASTContext> m_ast_ctx;
 };
 
 int main(int argc, char **argv)
